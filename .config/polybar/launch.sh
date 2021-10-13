45af86b1d21a1def387f94323cf2edb475c1cf55
z89 <z89@matix.com.au>
2021-10-14 00:24:37 +1000

    modified polybar's launch.sh for more reliability

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,13 +7,9 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
  
 # Launch bar1 and bar2
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload z89 &
-  done
-else
-  polybar --reload z89 &
-fi
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload taskbar &
+done
 
 #symlink spotify config
 ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-bottom
