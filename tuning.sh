2d2de30557ef351e97de8d997d872fb5d33e2314
z89 <n10234977@qut.edu.au>
2021-10-28 23:46:40 +1000

    included mouse sensitivity configurations for razer mamba (both wired & wireless modes)

--- tuning.sh
+++ tuning.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# typematic rate [time: 200ms, repeat rate: 40p/s]
 xset r rate 200 40
+
+# wirless mode speed // specific razer mouse configuration for razer mamba
+#xinput set-prop "pointer:Razer Razer Mamba Dock" "libinput Accel Speed" -1
+
+# wired mode // specific razer mouse configuration for razer mamba
+xinput set-prop "pointer:Razer Razer Mamba" "libinput Accel Speed" -1
