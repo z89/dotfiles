1e37d223a7d85ddc4ba5ff59b78adb5235fc7ed3
z89 <z89@matix.com.au>
2021-11-10 18:05:27 +1000

    refactor of polybar config & launch scripts w/o spotify module

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,12 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
  
-# Launch bar1 and bar2
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload taskbar &
-done
+MONITOR=DVI-D-0 polybar --reload primary &
+MONITOR=HDMI-0 polybar --reload secondary &
 
 #symlink spotify config
-ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-bottom
+#ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-bottom
 
-echo message >/tmp/ipc-bottom
+#echo message >/tmp/ipc-bottom
