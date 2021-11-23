4656c6d9a19b77693f11e1689ac68e179e355e9a
z89 <z89@matix.com.au>
2021-11-23 18:39:01 +1000

    adjusted desc in tuning.sh & updated package.list

--- tuning.sh
+++ tuning.sh
@@ -3,8 +3,8 @@
 # typematic rate [time: 200ms, repeat rate: 40p/s]
 xset r rate 200 40
 
-# wirless mode speed // specific razer mouse configuration for razer mamba
+# wireless mode // configuration for razer mamba mouse while wireless (undocked)
 xinput set-prop "pointer:Razer Razer Mamba Dock" "libinput Accel Speed" -1
 
-# wired mode // specific razer mouse configuration for razer mamba
+# wired mode // configuration for razer mamba mouse while wired (docked)
 xinput set-prop "pointer:Razer Razer Mamba" "libinput Accel Speed" -1
