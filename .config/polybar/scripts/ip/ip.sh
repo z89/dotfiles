eff0ee6429b0fce1bf14cbadfad62c81d2566ec2
z89 <z89@matix.com.au>
2022-07-06 02:43:42 +1000

    updated ip module script to display public/LAN IP only

--- .config/polybar/scripts/ip/ip.sh
+++ .config/polybar/scripts/ip/ip.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 publicIP="  $(curl -s https://ipinfo.io/ip)"
-localIP="  $(ip -4 addr show wlan0 | grep -oP "(?<=inet ).*(?=/)")"
-interface="  $(nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\' -f2 | cut -c 5-)"
-mem="$(cat ./.mem)"
+ethIP=$(ip -4 addr show enp5s0 | grep -oP "(?<=inet ).*(?=/)") 
+wlanIP=$(ip -4 addr show enp5s0 | grep -oP "(?<=inet ).*(?=/)")
 
+if [ -z "$ethIP" ]
+	then
+		localIP="  $(ip -4 addr show wlan0 | grep -oP "(?<=inet ).*(?=/)")"
+	else
+		localIP="  $(ip -4 addr show enp5s0 | grep -oP "(?<=inet ).*(?=/)")"
+fi
+mem="$(cat ./.mem)"
 
 case "$1" in 
     -u|--update)
@@ -22,17 +28,12 @@ case "$1" in
         fi
     ;;
 
-    -s|--ssid)
-        echo "$interface"
-    ;;
-
     -h|--help)
         echo "ip.sh created by z89 for polybar"
         echo " "
         echo "-h or --help          display a list of commands for ip.sh"
         echo "-u or --update        switches the ip address from public to local or vice versa"
         echo "-f or --fetch         displays the current ip address dependant on the state"
-        echo "-s or --ssid          displays the currently network ssid (broken, only set to wlp2s0 atm)"
     ;;
 
     *)
