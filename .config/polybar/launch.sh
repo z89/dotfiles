b1743e824b8cba5aff3153eba62584a1a30e406b
z89 <z89@matix.com.au>
2022-07-11 12:15:54 +1000

    changed polybar xrdb colors to a wpgtk generated template

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,8 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
  
-MONITOR=DVI-D-0 polybar --reload primary &
-MONITOR=HDMI-0 polybar --reload secondary &
+polybar --reload primary &
+polybar --reload secondary &
 
 #symlink spotify config
 #ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-bottom
