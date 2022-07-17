22bc658ac51ff2e762c995831e00e994c9d7d87b
z89 <n10234977@qut.edu.au>
2022-07-17 12:07:21 +1000

    properly fixed generated polybar template

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,7 +5,8 @@ killall -q polybar
  
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
- 
+
+polybar --reload wpgtk & 
 polybar --reload primary &
 polybar --reload secondary &
 
