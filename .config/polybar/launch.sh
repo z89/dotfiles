3c50cccee0bfdb78b75b65b9b5fbeacc699a239a
z89 <z89@matix.com.au>
2025-01-11 00:16:35 +1000

    polybar launch script refactor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env sh
  
-# Terminate already running bar instances
 killall -q polybar
- 
-# Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
 
 polybar --reload primary &
-polybar --reload secondary &
-
-#symlink spotify config
-#ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-bottom
-
-#echo message >/tmp/ipc-bottom
